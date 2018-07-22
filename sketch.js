faeb3309b40bc7bb8d13dee34db76182186e2bfa
Collin Bates <244503@bwls.net>
2018-07-21 22:05:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,20 +44,19 @@ var Ball = function(){
   this.drawBall = function( one , two ){
     this.dis = B[i].pos.dist(B[v].pos);
     
-    // var mid = createVector( width*0.5 , height*0.5 );
-    //
-    // var maxDist = (width*width) + (height*height);
-    //
-    // var dist = B[i].pos.dist( mid );
-    //
-    // var C = color( dist / maxDist * 1000000 %360 , 100 , 100 );
+    var mid = createVector( width*0.5 , height*0.5 );
+    
+    var maxDist = (width*width) + (height*height);
+    
+    var dist = B[i].pos.dist( mid );
+    
+    var C = color( dist / maxDist * 1000000 %360 , 100 , 100 );
     
     if( this.dis <= 100 ){
-      // stroke( C );
-      stroke( 0 , 0 , 100 );
+      stroke( C );
+      // stroke( 0 , 0 , 100 );
       line( B[i].pos.x , B[i].pos.y , B[v].pos.x , B[v].pos.y );
     }
   };
   
 };
-//bates stole this from jack ;P lol lol lol lol lol lol lol
